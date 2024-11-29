181d64fd31af2b8dbca1a21622e91cc0ab3d6a9b
yevhenii125 <yholub125@gmail.com>
2024-11-28 21:39:23 -0800

    commit

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  await app.listen(3000);
-  console.log('Invoice service is running on http://localhost:3000');
+  await app.listen(3005);
+  console.log('Invoice service is running on http://localhost:3005');
 }
 bootstrap();
